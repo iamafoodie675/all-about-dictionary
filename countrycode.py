75b041fe1f32751ca024f078a1db94833bc3d67f
nabila ahmed <sumayaad2020@gmail.com>
2024-09-27 19:38:34 +0600

    abc

---
+++ countrycode.py
@@ -0,0 +1,15 @@
+country_code ={
+    'India' : '0091',
+    'Australia': '0025',
+    'Bangladesh': '00880',
+    'Nepal' :'00977'
+    }
+
+print("country code for India -")
+print(country_code.get('India','Not found'))
+
+print("country code for Bangladesh -")
+print(country_code.get('Bangladesh','Not found'))
+
+print("country code for Japan -")
+print(country_code.get('Japan','Not found'))
